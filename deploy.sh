c6bae710b14314b81d500bdf4f0c3186397b115b
Celeste6666 <aafree2014@gmail.com>
2021-12-14 00:14:32 +0800

    作品集更新

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ set -e
 
 # git init
 git add -A
-git commit -m '簡單完成搜尋功能'
+git commit -m '作品集更新'
 
 # 部署到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
